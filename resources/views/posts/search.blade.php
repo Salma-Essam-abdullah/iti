7cf855bb95c477aea24dc3ae9066a6f044c13c52
Engy Ahmed Abdelaziz <engya306@gmail.com>
2022-08-21 17:30:57 +0200

    search

--- resources/views/posts/search.blade.php
+++ resources/views/posts/search.blade.php
@@ -1,12 +1,15 @@
 @section('title', "Search Result")
+
 @include('layouts.app2')
 @foreach($posts as $post)
 
 <div class="card" style="width: 35rem; margin:50px;">
   <img src="{{ asset('storage/'.$post->image) }}" class="card-img-top" alt="{{ $post->caption }}" >
   <div class="card-body">
+    <!-- username -->
     <h5 class="card-title">{{ $post->user->username }}</h5>
     <p class="card-text">{{ $post->caption }}</p>
+    <p>{{ $post->created_at->diffForHumans() }}</p>
   </div>
 </div>
 @endforeach
