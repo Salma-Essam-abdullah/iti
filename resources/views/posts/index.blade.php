e8c86c8fef77ca9c97661cac3cc510de5a3ecd34
Engy Ahmed Abdelaziz <engya306@gmail.com>
2022-08-23 13:23:13 +0200

    DisplayPostsThatUserHasFollow

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -64,9 +64,12 @@
     </style>
 </head>
 <body>
-@if(auth()->user()->follows()->count() > 0)
-    @foreach(auth()->user()->follows as $follow)
-@foreach($posts as $post)
+@if(auth()->user()->follows->count() == 0 && auth()->user()->posts()->count() == 0)
+<div class="alert alert-danger">
+  <strong>You don't have any post</strong>
+</div>
+@elseif(auth()->user()->follows->count() == 0 && auth()->user()->posts()->count() > 0)
+@foreach(auth()->user()->posts as $post)
 <div class="container posts-content">
     <div class="row">
         <div class="col-lg-6">
@@ -195,14 +198,135 @@
     </div>
 </div>
 @endforeach
-@endforeach
 @else
-    <div class="card">
-        <div class="card-body">
-            <p>You have no posts</p>
+@foreach(auth()->user()->follows as $follow)
+@foreach($follow->posts as $post)
+<div class="container posts-content">
+    <div class="row">
+        <div class="col-lg-6">
+            <div class="card mb-4">
+              <div class="card-body">
+                <div class="media mb-3">
+                <img class="d-block ui-w-40 rounded-circle"  src="{{Storage::disk('public')->url('/images//'.$post['user']['profile']['image'])}}"  class="img-thumbnail" width="100" height="100">
+                  <div class="media-body ml-3">
+                  {{$post['user']['name']}}
+                  </div>
+                </div>
+
+                <p>
+                <span class="text-muted">
+                                    @php
+                                        $cap =$post['caption'];
+                                        foreach($tags as $tag){
+                                        preg_match_all('/#(\w+)/', $cap, $matches);
+                                        $ncap = str_replace($tag->name,"<a href='/tags/$tag->id '>$tag->name</a>",$cap);
+                                        $cap = $ncap;
+                                        }
+                                        echo"$cap";
+                                    @endphp
+
+
+                                </span>
+                </p>
+
+                <a  href="{{ route('posts.show',$post['id']) }}" >  <img  src="{{Storage::disk('public')->url('/images//'.$post->images[0]->url)}}" alt="{{$post->caption}}" width="100%" height="100%"></a>
+            <br>
+                <form action="{{url('save' ,$post->id)}}" method="POST">
+                    @csrf
+                <input type="submit" class="btn btn-secondary"value="Save"/>
+                </form>
+
+              </div>
+              <div class="card-body">
+
+                @php
+                $like_count = 0;
+                $dislike_count = 0;
+                $like_status = "btn-secondary";
+                $dislike_status = "btn-secondary";
+                @endphp
+               @foreach($post['likes'] as $like)
+               @if($like['like'] == 1)
+               @php
+                $like_count++;
+                @endphp
+                @endif
+                @if($like['like'] == 0)
+                @php
+                  $dislike_count++;
+                @endphp
+                  @endif
+                @php
+
+                if($like['user_id'] == Auth::user()->id)
+                {
+                  if($like['like'] == 1)
+                  {
+                    $like_status = "btn-success";
+                  }
+                  else
+                  {
+                    $dislike_status = "btn-danger";
+                  }
+                }
+                @endphp
+
+               @endforeach
+               <hr>
+               <button  type="button" data-postid ="{{$post['id']}}_l" class="btn {{$like_status}} like" data-like="{{$like_status}}" >Like <i class="fa-solid fa-heart"></i><b><span class="like_count">{{$like_count}}</span></b></button>
+               <button type="button"  data-postid ="{{$post['id']}}_d" class="btn {{$dislike_status}} dislike" data-like="{{$dislike_status}}">Dislike <i class="fa-solid fa-heart-crack"></i><b><span class="dislike_count">{{$dislike_count}}</span><b></button>
+
+
+            </div>
+            <form method="POST" action="{{url('save-comment/'.$post['id'])}}">
+                @csrf
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <textarea class="form-control" rows="3" name="comment"></textarea>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                    <button type="submit" class="btn btn-success">Add Comment</button>
+                </div>
+            </form>
+            <div class="card my-4">
+                <h5 class="card-header">Comments : <span >{{count($post['comments'])}}</span></h5>
+                <div class="card-body">
+                     @if($post['comments'])
+
+                     @foreach($post['comments']->take(3) as $comment)
+                     <figure>
+                         <blockquote class="blockquote">
+
+                            <img class="d-block ui-w-40 rounded-circle"  src="{{Storage::disk('public')->url('/images//'.$post['user']['profile']['image'])}}"  class="img-thumbnail" width="100" height="100">
+
+                             {{$post['user']['name']}}
+                         </blockquote>
+                         <figcaption class="blockquote-footer">
+
+                             <h3>{{$comment['comment']}}</h3>
+                             <p> Time : {{$comment['created_at']}}</p>
+                         </figcaption>
+                       </figure>
+
+                     @endforeach
+                     @if($post['comments']->count() > 3)
+
+                     <a href="{{ route('posts.show',$post['id']) }}"> See More Comments</a>
+                      @endif
+                     @endif
+                     <br>
+            </div>
+        </div>
+            </div>
         </div>
     </div>
+</div>
+
+@endforeach
+@endforeach
 @endif
+
 </body>
 </html>
 @endsection
